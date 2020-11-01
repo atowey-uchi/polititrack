9797b0298742f53efbed7481d6f564e2d27c7635
Marcus Schimizzi <marcusjschimizzi@gmail.com>
2020-10-31 18:53:39 -0700

    pie chart

---
+++ src/components/Pie.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="pie-chart">
+    <!-- Add 2 buttons -->
+    <button @click="data = data1">Data 1</button>
+    <button @click="data = data2">Data 2</button>
+
+    <!-- Create a div where the graph will take place -->
+    <div id="pie">
+      <svg :width="width" :height="height">
+        <transition-group
+          tag="g"
+          :transform="transform"
+          class="slices"
+          name="slices"
+        >
+          <path
+            v-for="slice in slices"
+            :id="slice.id"
+            :key="slice.id"
+            :fill="slice.fill"
+            :d="slice.d"
+            :stroke="slice.stroke"
+            :style="slice.style"
+          ></path>
+        </transition-group>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+  name: "Pie",
+  data() {
+    return {
+      width: 450,
+      height: 450,
+      margin: 40,
+      transform: "",
+      data1: [
+        {
+          name: "a",
+          value: 9
+        },
+        {
+          name: "b",
+          value: 20
+        },
+        {
+          name: "c",
+          value: 30
+        },
+        {
+          name: "d",
+          value: 8
+        },
+        {
+          name: "e",
+          value: 12
+        }
+      ],
+      data2: [
+        {
+          name: "a",
+          value: 6
+        },
+        {
+          name: "b",
+          value: 16
+        },
+        {
+          name: "c",
+          value: 20
+        },
+        {
+          name: "d",
+          value: 14
+        },
+        {
+          name: "e",
+          value: 19
+        },
+        {
+          name: "f",
+          value: 12
+        }
+      ],
+      colors: "",
+      radius: 0,
+      data: []
+    };
+  },
+  methods: {
+    setupChart() {
+      this.colors = d3
+        .scaleOrdinal()
+        .domain(["a", "b", "c", "d", "e", "f"])
+        .range(d3.schemeDark2);
+
+      this.transform = `translate(${this.width / 2}, ${this.height / 2})`;
+      this.radius = Math.min(this.width, this.height) / 2 - this.margin;
+      this.data = this.data1;
+    }
+  },
+  created() {
+    this.setupChart();
+  },
+  computed: {
+    slices() {
+      if (this.data && this.colors) {
+        console.log(this.data);
+        let data = d3.pie().value(d => d.value)(this.data);
+        console.info(data);
+        return data.map((d, i) => {
+          return {
+            id: i,
+            d: d3
+              .arc()
+              .innerRadius(0)
+              .outerRadius(this.radius)
+              .startAngle(d.startAngle)
+              .endAngle(d.endAngle)(),
+            fill: this.colors(d.value),
+            stroke: "white",
+            style: {
+              strokeWidth: "2px",
+              opacity: 1
+            }
+          };
+        });
+      }
+      return [];
+    }
+  }
+};
+</script>
+
+<style></style>
